4beaf351ab756e2b03b6364f1357a4ae1335782f
Eugene Shuvaev <aidden@transas.com>
2018-03-15 17:37:20 +0300

    combine scripts into one

---
+++ NodePrepare.ps1
@@ -0,0 +1,97 @@
+param([Parameter(Mandatory=$true)][string]$chocoPackages)
+cls
+
+# Expand OS disk
+foreach($disk in Get-Disk)
+{
+    # Check if the disk in context is a Boot and System disk
+    if((Get-Disk -Number $disk.number).IsBoot -And (Get-Disk -Number $disk.number).IsSystem)
+    {
+        # Get the drive letter assigned to the disk partition where OS is installed
+        $driveLetter = (Get-Partition -DiskNumber $disk.Number | where {$_.DriveLetter}).DriveLetter
+        Write-verbose "Current OS Drive: $driveLetter :\"
+
+        # Get current size of the OS parition on the Disk
+        $currentOSDiskSize = (Get-Partition -DriveLetter $driveLetter).Size        
+        Write-verbose "Current OS Partition Size: $currentOSDiskSize"
+
+        # Get Partition Number of the OS partition on the Disk
+        $partitionNum = (Get-Partition -DriveLetter $driveLetter).PartitionNumber
+        Write-verbose "Current OS Partition Number: $partitionNum"
+
+        # Get the available unallocated disk space size
+        $unallocatedDiskSize = (Get-Disk -Number $disk.number).LargestFreeExtent
+        Write-verbose "Total Unallocated Space Available: $unallocatedDiskSize"
+
+        # Get the max allowed size for the OS Partition on the disk
+        $allowedSize = (Get-PartitionSupportedSize -DiskNumber $disk.Number -PartitionNumber $partitionNum).SizeMax
+        Write-verbose "Total Partition Size allowed: $allowedSize"
+
+        if ($unallocatedDiskSize -gt 0 -And $unallocatedDiskSize -le $allowedSize)
+        {
+            $totalDiskSize = $allowedSize
+            
+            # Resize the OS Partition to Include the entire Unallocated disk space
+            $resizeOp = Resize-Partition -DriveLetter C -Size $totalDiskSize
+            Write-verbose "OS Drive Resize Completed $resizeOp"
+        }
+        else {
+            Write-Verbose "There is no Unallocated space to extend OS Drive Partition size"
+        }
+    }   
+}
+
+# Get username/password & machine name
+$userName = "artifactInstaller"
+[Reflection.Assembly]::LoadWithPartialName("System.Web") | Out-Null
+$password = $([System.Web.Security.Membership]::GeneratePassword(12,4))
+$cn = [ADSI]"WinNT://$env:ComputerName"
+
+# Create new user
+$user = $cn.Create("User", $userName)
+$user.SetPassword($password)
+$user.SetInfo()
+$user.description = "Choco artifact installer"
+$user.SetInfo()
+
+# Add user to the Administrators group
+$group = [ADSI]"WinNT://$env:ComputerName/Administrators,group"
+$group.add("WinNT://$env:ComputerName/$userName")
+
+# Create pwd and new $creds for remoting
+$secPassword = ConvertTo-SecureString $password -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\$($username)", $secPassword)
+
+# Ensure that current process can run scripts. 
+#"Enabling remoting" | Out-File $LogFile -Append
+Enable-PSRemoting -Force -SkipNetworkProfileCheck
+
+#"Changing ExecutionPolicy" | Out-File $LogFile -Append
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
+
+# Install Choco
+#"Installing Chocolatey" | Out-File $LogFile -Append
+$sb = { iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')) }
+Invoke-Command -ScriptBlock $sb -ComputerName $env:COMPUTERNAME -Credential $credential | Out-Null
+
+#"Disabling UAC" | Out-File $LogFile -Append
+$sb = { Set-ItemProperty -path HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System -name EnableLua -value 0 }
+Invoke-Command -ScriptBlock $sb -ComputerName $env:COMPUTERNAME -Credential $credential
+
+#"Install each Chocolatey Package"
+$chocoPackages.Split(";") | ForEach {
+    $command = "cinst " + $_ + " -y -force"
+    $command | Out-File $LogFile -Append
+    $sb = [scriptblock]::Create("$command")
+
+    # Use the current user profile
+    Invoke-Command -ScriptBlock $sb -ArgumentList $chocoPackages -ComputerName $env:COMPUTERNAME -Credential $credential | Out-Null
+}
+
+Disable-PSRemoting -Force
+
+# Delete the artifactInstaller user
+$cn.Delete("User", $userName)
+
+# Delete the artifactInstaller user profile
+gwmi win32_userprofile | where { $_.LocalPath -like "*$userName*" } | foreach { $_.Delete() }
